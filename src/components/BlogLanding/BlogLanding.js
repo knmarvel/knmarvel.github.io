8199381c796078a719db284c78459dc444b9753d
Kano Marvel <knmarvel@gmail.com>
2021-02-09 18:47:18 -0500

    * Imported [react-spinners](https://www.npmjs.com/package/react-spinners) library so if my blog doesn't immediately load, we've still got something visually interesting on the page.     * [UUID for the keys](https://www.npmjs.com/package/uuid)

--- src/components/BlogLanding/BlogLanding.js
+++ src/components/BlogLanding/BlogLanding.js
@@ -2,18 +2,27 @@ import {
     BlogPostShort
 } from "../"
 import "./BlogLanding.css"
+import { css } from "@emotion/core";
 import blogs from "../../assets/blogs/blogs.json"
+import MoonLoader from "react-spinners/MoonLoader";
+import { v4 as uuidv4 } from 'uuid';
 
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 export default function BlogLanding(){
     return(<>
             {blogs && 
                 <div>
-                   
-                    {blogs.blogs.map((blog, key) => <BlogPostShort key={key} id={key + 1} blog={blog}/>)}
+                    {blogs.blogs.map((blog, key) => <BlogPostShort key={uuidv4()} id={key + 1} blog={blog}/>)}
                 </div>
             }
-            
+            <MoonLoader color="#FFFFFF" css={override} loading={!blogs}/>
+
             </>
     )
 }
